d79841760368a8ff4447a8845d7fd0e6a81b5a14
Douglas Ullmann <111387389+daugbit@users.noreply.github.com>
2022-09-29 16:06:19 -0300

    Update cnpj.py

--- Python 3 (Udemy)/ex019_validador_CNPJ/cnpj.py
+++ Python 3 (Udemy)/ex019_validador_CNPJ/cnpj.py
@@ -1,90 +1,47 @@
+aux = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+
 def format(cnpj):
     """
-    Função que formata o CNPJ informado pelo usuário, removendo eventuais pontuações
+    Função que formata o CNPJ informado pelo usuário, removendo eventuais pontuações, e verifica se o CNPJ não é uma sequência
     param. cnpj: número do CNPJ digitado pelo usuário
     return: número do CNPJ formatado como 'xxxxxxxxxxxxxx'
     """
     cnpj = cnpj.replace('.', '').replace('/', '').replace('-', '')
-    return cnpj
+    if cnpj == cnpj[0] * 14:
+        return False
+    else:
+        return cnpj
 
 
-def sequenceone(cnpj):
-    """
-    Função que cria a sequência auxiliar que será utilizada para cálculo do primeiro dígito
-    param. cnpj: número do CNPJ no formato de lista de dígitos, sem os dígitos verificadores
-    return: lista auxiliar para o cálculo
-    """
-    aux = []
-    num = 5
-    for i in cnpj:
-        if num >= 2:
-            aux.append(str(num))
-        else:
-            num = 9
-            aux.append(str(num))
-        num -= 1
-    return aux
-
-
-def calcdigitone(raw, aux):
+def calcdigitone(cnpj):
     """
     Função que calcula o primeiro verificador e insere na lista da sequência
-    param. raw: lista de dígitos (sem os dígitos verificadores) do CNPJ informado pelo usuário
-    param. aux: lista auxiliar
+    param. cnpj: lista de dígitos (sem os dígitos verificadores) do CNPJ informado pelo usuário
     return: lista de dígitos, agora com o primeiro dígito verificador, do CNPJ informado pelo usuário
     """
     num = 0
     for i in range(12):
-        calc = int(raw[i]) * int(aux[i])
+        calc = int(cnpj[i]) * aux[i + 1]
         num += calc
     dig = 11 - (num % 11)
     if dig > 9:
         dig = 0
-    raw.append(str(dig))
-    return raw
+    cnpj = cnpj + str(dig)
+    return cnpj
 
 
-def sequencetwo(cnpj):
-    """
-    Função que cria a sequência auxiliar que será utilizada para cálculo do segundo dígito
-    param. cnpj: número do CNPJ no formato de lista de dígitos, apens com o primeiro dígito verificador
-    return: segundo lista auxiliar para o cálculo
-    """
-    aux = []
-    num = 6
-    for i in cnpj:
-        if num >= 2:
-            aux.append(str(num))
-        else:
-            num = 9
-            aux.append(str(num))
-        num -= 1
-    return aux
-
-
-def calcdigittwo(raw, aux):
+def calcdigittwo(cnpj):
     """
     Função que calcula o segundo verificador e insere na lista da sequência
-    param. raw: lista de dígitos (apenas com o primeiro dígito verificador calculado) do CNPJ informado pelo usuário
-    param. aux: segunda lista auxiliar
+    param. cnpj: lista de dígitos (apenas com o primeiro dígito verificador calculado) do CNPJ informado pelo usuário
     return: lista de dígitos, agora com os dois dígitos verificadores calculados, do CNPJ informado pelo usuário
     """
     num = 0
     for i in range(13):
-        calc = int(raw[i]) * int(aux[i])
+        calc = int(cnpj[i]) * aux[i]
         num += calc
     dig = 11 - (num % 11)
     if dig > 9:
         dig = 0
-    raw.append(str(dig))
-    return raw
-
-
-def reformat(cnpj):
-    """
-    Função que faz join dos dígitos da lista, formatando o CNPJ completo (apenas números)
-    param. cnpj: lista de dígitos, com os dois dígitos verificadores calculados, do CNPJ informado pelo usuário
-    return: número do CNPJ no formato 'xxxxxxxxxxxxxx'
-    """
-    cnpj = ''.join(cnpj)
+    cnpj = cnpj + str(dig)
     return cnpj
