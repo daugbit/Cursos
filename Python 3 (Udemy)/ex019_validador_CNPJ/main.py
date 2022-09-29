dd51c3df760bce81985a8100bdd46b468175a968
Douglas Ullmann <111387389+daugbit@users.noreply.github.com>
2022-09-29 16:29:19 -0300

    Update main.py

--- Python 3 (Udemy)/ex019_validador_CNPJ/main.py
+++ Python 3 (Udemy)/ex019_validador_CNPJ/main.py
@@ -5,14 +5,19 @@ import cnpj
 
 try:
     cnpj_input = cnpj.format(str(input('CNPJ: ')))
-    raw_cnpj = cnpj_input[:-2]
-    cnpj1 = cnpj.calcdigitone(raw_cnpj)
-    cnpj2 = cnpj.calcdigittwo(cnpj1)
-    
-    if cnpj_input == cnpj2:
-        print('\033[1:30:42m', 'CNPJ VÁLIDO!'.center(60), '\033[m')
-    else:
-        print('\033[1:30:41m', 'CNPJ INVÁLIDO!'.center(60), '\033[m')
-        
-except (ValueError, IndexError):
+except ValueError:
     print('Formato de CPJ inválido!')
+else:
+    if not cnpj_input:
+        print('Formato de CNPJ inválido!')
+    else:
+        raw_cnpj = cnpj_input[:-2]
+        cnpj1 = cnpj.calcdigitone(raw_cnpj)
+        cnpj2 = cnpj.calcdigittwo(cnpj1)
+
+        if cnpj_input == cnpj2:
+            print('\033[1:30:42m', 'CNPJ VÁLIDO!'.center(60), '\033[m')
+        else:
+            print('\033[1:30:41m', 'CNPJ INVÁLIDO!'.center(60), '\033[m')
+        
+
