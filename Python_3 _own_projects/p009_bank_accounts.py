f142d120766393625effc28ee04d9f34be2b3a1a
Douglas Ullmann <doaull@gmail.com>
2022-10-08 15:56:34 -0300

    Update p009_bank_accounts.py

--- Python_3 _own_projects/p009_bank_accounts.py
+++ Python_3 _own_projects/p009_bank_accounts.py
@@ -1,57 +1,71 @@
-Class Conta:
-	def __init__(self, number, type, owner)
-	  self.number = 
-	  self.type = 
-	  self.owner = 
-	
+class Conta:
+	def __init__(self, a_number, a_type, a_owner):
+		self.number = a_number
+		self.type = a_type
+		self.owner = a_owner
+		self.a_status = None
+		self.a_type = None
+		self.a_balance = None
 
-	def open_account():
-		self.status = True
+	def open_account(self):
+		self.a_status = True
 		if self.type == 'corrente':
-			self.balance = 50
+			self.a_balance = 50
 		elif self.type == 'poupança':
-			self.balance = 150
+			self.a_balance = 150
 
-
-	def close_account():
-		if self.balance != 0:
+	def close_account(self):
+		if self.a_balance != 0:
 			print('Esta conta não pode ser encerrada, pois possui saldo diferente de R$0')
 		else:
-			self.status = False
+			self.a_status = False
 
-
-	def deposit(value):
-		if self.status == False:
+	def deposit(self, value):
+		if not self.a_status:
 			print('Conta desativada')
 			return None
 		
-		self.balance += value
-	
+		self.a_balance += value
 
-	def withdraw(value):
-		if self.status == False:
+	def withdraw(self, value):
+		if not self.a_status:
 			print('Conta desativada')
 			return None
 		
-		if value > self.balance:
+		if value > self.a_balance:
 			print('O valor informado é maior que o saldo disponível')
 		else:
-			self.balance -= value
+			self.a_balance -= value
 
-
-	def pay_fee():
-		if self.status == False:
+	def pay_fee(self):
+		if not self.a_status:
 			print('Conta desativada')
 			return None
 		
 		if self.type == 'corrente':
-			self.balance -= 20
+			self.a_balance -= 20
 		elif self.type == 'poupança':
-			self.balance -= 10
+			self.a_balance -= 10
 
 
+while True:
+	print('=-' * 30)
+	print('DAU Bank'.center(60))
+	print('=-' * 30)
+	print('[1] Consultar conta')
+	print('[2] Abrir nova conta')
+	print('[3] Encerrar conta')
+	print('[4] Depósitar')
+	print('[5] Sacar')
+	print('[6] SAIR')
+	print('=-' * 30)
+	opt = input('Digite uma das opções: ')
 
-client1 = Conta(001, 'corrente', 'Douglas')
+	if opt == '1':
+		print()
+
+
+client1 = Conta('001', 'corrente', 'Douglas')
 
 client1.open_account()
 
@@ -59,6 +73,6 @@ client1.deposit(100)
 
 client1.withdraw(130)
 
-client1.pay_fee(20)
+client1.pay_fee()
 
 client1.close_account()
